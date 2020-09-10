172c8f525a38340b9249efbda5aa0b4414e6689a
Nekke <pablonekketsu@gmail.com>
2020-09-10 14:35:37 -0500

    Primer push

--- app/src/main/java/com/example/prueba/ConectarSQLite.java
+++ app/src/main/java/com/example/prueba/ConectarSQLite.java
@@ -9,6 +9,7 @@ public class ConectarSQLite extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conectar_s_q_lite);
         ConexionSQLiteHelper conector= new ConexionSQLiteHelper(getApplicationContext(),"db.usuario",null,1);
